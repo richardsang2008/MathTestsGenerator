5fc4bac49cdad28984d64c86af579a71ad65dcdd
richard sang <richardsang2008@gmail.com>
2018-12-04 23:34:41 -0800

    more log handle

--- controllers/StudentController.go
+++ controllers/StudentController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/json"
 	"github.com/jinzhu/gorm"
 	"github.com/richardsang2008/MathTestsGenerator/models/requests"
 	"github.com/richardsang2008/MathTestsGenerator/models/response"
@@ -42,10 +43,19 @@ func (r *StudentController) CreateStudent(c *gin.Context) {
 	c.BindJSON(&newstudent)
 	isValid, err := newstudent.IsValid()
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": fmt.Sprintf("new student validation has error %s ", err)})
+		r.log.Error(fmt.Sprintf("CreateStudent input error %s",err))
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
 	}
 	if isValid == false {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "new student validation failed %s "})
+		r.log.Error(fmt.Sprintf("CreateStudent input is not valid"))
+		c.JSON(http.StatusBadRequest, gin.H{"message": "new student validation failed %s "})
+	}
+	bytes,err := json.Marshal(newstudent)
+	if err != nil {
+		r.log.Error(fmt.Sprintf("CreateStudent input error %s",err))
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+	} else {
+		r.log.Info(fmt.Sprintf("CreateStudent input %s",string(bytes)))
 	}
 	studentId:= generateRandomString(12)
 	student:= compositemodels.Student{FirstName:newstudent.FName,MidName:newstudent.MName,LastName:newstudent.LName,Email:newstudent.Email,StudentId:studentId}
@@ -53,22 +63,35 @@ func (r *StudentController) CreateStudent(c *gin.Context) {
 	if sid >0{
 		retStudent:= response.StudentInfo{FName:student.FirstName,MName:student.MidName,LName:student.LastName,
 			Email:student.Email,StudentId:student.StudentId,Id:sid, EnrollmentDate:student.EnrollmentDate}
-		c.JSON(http.StatusOK, retStudent)
+		bytes,err = json.Marshal(retStudent)
+		if err != nil {
+			r.log.Error(fmt.Sprintf("CreateStudent response error %s",err))
+			c.JSON(http.StatusBadRequest, gin.H{"message": err})
+		} else {
+			r.log.Info(fmt.Sprintf("CreateStudent response %s", string(bytes)))
+			c.JSON(http.StatusOK, retStudent)
+		}
+
 	} else{
+		r.log.Info("CreateStudent response email is used")
 		c.JSON(http.StatusNotFound,gin.H{"message":"Email is used"})
 	}
 }
 
 func (r *StudentController) GetStudentByStudentId(c *gin.Context) {
 	studentId := c.DefaultQuery("studnetId", "NULL")
+	r.log.Info(fmt.Sprintf("GetStudentByStudentId input %s",studentId))
 	if studentId != "NULL" && studentId != "\"\"" {
 		student:=r.Repository.GetStudentByStudentId(studentId)
 		if student.Email=="" {
+			r.log.Info(fmt.Sprintf("GetStudentByStudentId return no student by input %s", studentId))
 			c.JSON(http.StatusNotFound,student)
 		} else {
+			r.log.Info("GetStudentByStudentId return OK")
 			c.JSON(http.StatusOK, student)
 		}
 	} else {
+		r.log.Error(fmt.Sprintf("GetStudentByStudentId input is empty %s", studentId))
 		c.JSON(http.StatusBadRequest, gin.H{"Error": "studnetId is empty"})
 	}
 
@@ -76,14 +99,24 @@ func (r *StudentController) GetStudentByStudentId(c *gin.Context) {
 
 func (r *StudentController) GetStudentByEmail(c *gin.Context) {
 	email := c.DefaultQuery("email", "NULL")
+	r.log.Info(fmt.Sprintf("GetStudentByEmail input %s", email))
 	if email != "NULL" && email != "\"\"" {
 		student:=r.Repository.GetStudentByEmail(email)
 		if student.Email=="" {
+			r.log.Info(fmt.Sprintf("GetStudentByEmail return no record by email %s",email))
 			c.JSON(http.StatusNotFound,student)
 		} else {
-			c.JSON(http.StatusOK, student)
+			bytes, err:= json.Marshal(student)
+			if err != nil {
+				r.log.Error(fmt.Sprintf("GetStudentByEmail return error %s",err))
+				c.JSON(http.StatusInternalServerError, gin.H{"message":err})
+			} else {
+				r.log.Info(fmt.Sprintf("GetStudentByEmail return %s", string(bytes)))
+				c.JSON(http.StatusOK, student)
+			}
 		}
 	} else {
+		r.log.Error("GetStudentByEmail input is empty")
 		c.JSON(http.StatusBadRequest, gin.H{"Error": "email is empty"})
 	}
 }
